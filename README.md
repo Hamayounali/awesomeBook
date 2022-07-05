59b311c5e4cb687a7c5c4ff0d45a8ed956267d6b
Hamayoun <hamayounemail@gmail.com>
2022-07-05 14:39:37 +0430

    Updated index.js style.css

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# awesomeBook
+# Awesome Books
+Online book library 
+
+## Awesome books screen shot
+
+Comming soom 
+
+## Page Link 
+https://hamayounali.github.io/KabulKia/
+
+# Setup
+Clone this repository
+
+$ git clone git@github.com:Hamayounali/awesomeBook.git
+$ cd kabbulkia
+
+Run the live server or open index.html with your browser
+
+# Built With
+
+• HTML, CSS and BootStrap
+• JavaScript
+• VSCode
+
+# Authors
+👤 Hamayoun Ali
+
+• GitHub: @hamayounali
+
+• Twitter: @hamayounali6
+
+
+# Show your support
+Give a ⭐️ if you like this project!
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,37 @@
 let bookArr = [];
 
 function input(){
-    let name = document.getElementById("name").value;
-    let author = document.getElementById("author").value;
-    bookArr.push({'bookName':name, 'author':author});
-    localStorage.setItem('books', JSON.stringify(bookArr));
-    location.reload();
+  let name = document.getElementById("name").value;
+  let author = document.getElementById("author").value;
+  bookArr.push({'bookName':name, 'author':author});
+  localStorage.setItem('books', JSON.stringify(bookArr));
+  location.reload();
 }
 
 function display() {
-    let html = '<div>';
-    let storedBook = JSON.parse(localStorage.getItem('books'));
-    for(let i = 0; i <storedBook.length;  i ++){
-        bookArr.push({'bookName':storedBook[i].bookName, 'author':storedBook[i].author});
-        html += '<p>Book Name: ' + bookArr[i].bookName +' <br> Author Name : '+ bookArr[i].author +'</p>' ;
-        const book = i;
-        html += '<button onclick="deleteNote('+book+')"> Remove </button>';
-        html += '<br>';
-        html += '<hr class ="line">';
-        html += '<br>';
-        document.getElementById('data').innerHTML=html;
-    }
+let html = '<div>';
+let storedBook = JSON.parse(localStorage.getItem('books'));
+  for(let i = 0; i <storedBook.length;  i ++){
+    bookArr.push({'bookName':storedBook[i].bookName, 'author':storedBook[i].author});
+    html += '<p>Book Name: ' + bookArr[i].bookName +' <br> Author Name : '+ bookArr[i].author +'</p>' ;
+    const book = i;
+    html += '<button onclick="deleteNote('+book+')"> Remove </button>';
+    html += '<br>';
+    html += '<hr class ="line">';
+    html += '<br>';
+    document.getElementById('data').innerHTML=html;
+  }
 }
 
 function deleteNote(i) {
-    bookArr.splice(i, 1);
-    localStorage.setItem('books', JSON.stringify(bookArr));
-    location.reload();
-  }
+  bookArr.splice(i, 1);
+  localStorage.setItem('books', JSON.stringify(bookArr));
+  location.reload();
+}
 
-  document.addEventListener('DOMContentLoaded', () => {
-    display();
-  });
+document.addEventListener('DOMContentLoaded', () => {
+  display();
+});
 
 
 
