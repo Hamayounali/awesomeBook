67df40db6fcfae492a856748257c79e85a24dbe9
Hamayoun <hamayounemail@gmail.com>
2022-07-05 15:03:01 +0430

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ const bookArr = [];
 function input() {
   const name = document.getElementById('name').value;
   const author = document.getElementById('author').value;
-  bookArr.push( { bookName : name, author : author } );
+  bookArr.push({ bookName: name, author: author });
   localStorage.setItem('books', JSON.stringify(bookArr));
   location.reload();
 }
@@ -11,15 +11,15 @@ function input() {
 function display() {
   let html = '<div>';
   const storedBook = JSON.parse(localStorage.getItem('books'));
-  for (let i = 0; i < storedBook.length; i +=1 ) {
-    bookArr.push( { bookName : storedBook[i].bookName, author : storedBook[i].author } );
-    html += '<p>Book Name: ' + bookArr[i].bookName + ' <br> Author Name : ' + bookArr[i].author + '</p>' ;
+  for (let i = 0; i < storedBook.length; i += 1 ) {
+    bookArr.push({ bookName: storedBook[i].bookName, author: storedBook[i].author });
+    html += '<p>Book Name: ' + bookArr[i].bookName + ' <br> Author Name : ' + bookArr[i].author + '</p>';
     const book = i;
     html += '<button onclick="deleteNote(' + book + ')"> Remove </button>';
     html += '<br>';
     html += '<hr class ="line">';
     html += '<br>';
-    document.getElementById('data').innerHTML=html;
+    document.getElementById('data').innerHTML = html;
   }
 }
 
@@ -32,6 +32,3 @@ function deleteNote(i) {
 document.addEventListener('DOMContentLoaded', () => {
   display();
 });
-
-
-
