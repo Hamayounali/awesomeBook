b55f4e7d37523aee689b9b0470283242bf95b088
Hamayoun <hamayounemail@gmail.com>
2022-07-05 14:55:27 +0430

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,21 @@
-let bookArr = [];
+const bookArr = [];
 
-function input(){
-  let name = document.getElementById("name").value;
-  let author = document.getElementById("author").value;
-  bookArr.push({'bookName':name, 'author':author});
+function input() {
+  const name = document.getElementById('name').value;
+  const author = document.getElementById('author').value;
+  bookArr.push( { bookName : name, author : author } );
   localStorage.setItem('books', JSON.stringify(bookArr));
   location.reload();
 }
 
 function display() {
-let html = '<div>';
-let storedBook = JSON.parse(localStorage.getItem('books'));
-  for(let i = 0; i <storedBook.length;  i ++){
-    bookArr.push({'bookName':storedBook[i].bookName, 'author':storedBook[i].author});
-    html += '<p>Book Name: ' + bookArr[i].bookName +' <br> Author Name : '+ bookArr[i].author +'</p>' ;
+  let html = '<div>';
+  const storedBook = JSON.parse(localStorage.getItem('books'));
+  for (let i = 0; i < storedBook.length; i +=1 ) {
+    bookArr.push( { bookName : storedBook[i].bookName, author : storedBook[i].author } );
+    html += '<p>Book Name: ' + bookArr[i].bookName + ' <br> Author Name : ' + bookArr[i].author + '</p>' ;
     const book = i;
-    html += '<button onclick="deleteNote('+book+')"> Remove </button>';
+    html += '<button onclick="deleteNote(' + book + ')"> Remove </button>';
     html += '<br>';
     html += '<hr class ="line">';
     html += '<br>';
